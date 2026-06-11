0c4e9280a9e4549eeda30e06e3ad448f25d9aca5
Hani <hanigxp@gmail.com>
2026-06-11 17:16:52 +0100

    voici l'exo 9 dernier jour

---
+++ exo9.rb
@@ -0,0 +1,16 @@
+#Mixins : Les modules
+#include : on gros on mais un systeme de module pour ajouter le meme systeme d'une methode et le rajouter dans un module
+#self.include
+#extend il transfere les methodes d'instance en methode de class c'est comme self  self..
+#self.extended 
+#each_wich_index : pour savoir ou elle est la methode 
+ 
+#Type error :
+#raise : faire une erreur et sauter la ligne
+#raise ArgumentError la ligne derreur par exemple
+#une ligne de code pour les ereurs :
+#begin se termine par end 
+#rescue Exception
+#on met rescue le problem qu'on a dans le terminal 
+#ensure : quooi qu'il arrive avec ce parametre la ligne de code va s'executer 
+#
